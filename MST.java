2be1abcb3c20decc567394557449367fcc326239
Shekhar Chaugule <79699567+shekhargit1912@users.noreply.github.com>
2021-06-03 22:00:02 +0530

    Create MST.java

---
+++ MST.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+class PGraph {
+
+  public void Prim(int G[][], int V) {
+
+    int INF = 9999999;
+
+    int no_edge; // number of edge
+
+    // create a array to track selected vertex
+    // selected will become true otherwise false
+    boolean[] selected = new boolean[V];
+
+    // set selected false initially
+    Arrays.fill(selected, false);
+
+    no_edge = 0;
+
+    selected[0] = true;
+
+    System.out.println("Edge : Weight");
+
+    while (no_edge < V - 1) {
+    
+      int min = INF;
+      int x = 0; // row number
+      int y = 0; // col number
+
+      for (int i = 0; i < V; i++) {
+        if (selected[i] == true) {
+          for (int j = 0; j < V; j++) {
+            // not in selected and there is an edge
+            if (!selected[j] && G[i][j] != 0) {
+              if (min > G[i][j]) {
+                min = G[i][j];
+                x = i;
+                y = j;
+              }
+            }
+          }
+        }
+      }
+      System.out.println(x + " - " + y + " :  " + G[x][y]);
+      selected[y] = true;
+      no_edge++;
+    }
+  }
+
+  public static void main(String[] args) {
+    PGraph g = new PGraph();
+
+    
+    int V = 5;
+
+    int[][] G = { { 0, 9, 75, 0, 0 }, { 9, 0, 95, 19, 42 }, { 75, 95, 0, 51, 66 }, { 0, 19, 51, 0, 31 },
+        { 0, 42, 66, 31, 0 } };
+
+    g.Prim(G, V);
+  }
+}
